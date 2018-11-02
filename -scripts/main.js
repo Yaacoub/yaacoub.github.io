37d7e2f19c0a2330be263abe10643029e94f5458
Peter Yaacoub <pwyaacoub@gmail.com>
2018-11-02 12:10:15 +0200

    Organise stylesheets; Remove overflow while fullscreen menu open

--- -scripts/main.js
+++ -scripts/main.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
     $("#openMenu").click(function () {
         $("#topMenu").hide();
         $("#sideMenu").toggleClass("fullscreenMenu");
+        $("body").toggleClass("noScroll");
     });
     
     $("#closeMenu").click(function () {
         $("#topMenu").show();
         $("#sideMenu").toggleClass("fullscreenMenu");
+        $("body").toggleClass("noScroll");
     });
 
 });

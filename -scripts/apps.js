ffc023ac9eb89d43f5d351d36ba3b9617868f59a
Peter Yaacoub <pwyaacoub@gmail.com>
2019-03-02 13:02:53 +0200

    Update index.html and apps.js

--- -scripts/apps.js
+++ -scripts/apps.js
@@ -4,23 +4,13 @@ $(document).ready(function () {
     $("#appsBackgroundOneHidden").hide();
 
     $("#appsBackgroundOneVisible").click(function() {
-        if ($("#appsBackgroundOneHidden").is(":hidden")) {
-            $("#appsBackgroundOneVisible").hide();
-            $("#appsBackgroundOneHidden").show();
-        } else {
-            $("#appsBackgroundOneVisible").show();
-            $("#appsBackgroundOneHidden").hide();
-        }
+        $("#appsBackgroundOneVisible").toggle();
+        $("#appsBackgroundOneHidden").toggle();
     });
 
     $("#appsBackgroundOneHidden").click(function() {
-        if ($("#appsBackgroundOneVisible").is(":hidden")) {
-            $("#appsBackgroundOneHidden").hide();
-            $("#appsBackgroundOneVisible").show();
-        } else {
-            $("#appsBackgroundOneHidden").show();
-            $("#appsBackgroundOneVisible").hide();
-        }
+        $("#appsBackgroundOneVisible").toggle();
+        $("#appsBackgroundOneHidden").toggle();
     });
 
 });

803158560e986910c8cfba3e3b4a6d4a4fbf9fb8
Peter Yaacoub <pwyaacoub@gmail.com>
2019-03-08 09:56:45 +0200

    • Update -global.js and -global.css.

--- -scripts/-global.js
+++ -scripts/-global.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
 
     $("#menu-open").click(function () {
+        $("#main").toggleClass("main-noscroll");
         $("#menu").toggleClass("menu-hidden");
         $("#menu-close").show();
         $("#menu-open").hide();
     });
     
     $("#menu-close").click(function () {
+        $("#main").toggleClass("main-noscroll");
         $("#menu").toggleClass("menu-hidden");
         $("#menu-close").hide();
         $("#menu-open").show();
